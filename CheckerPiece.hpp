e0ea023f29287f52d2b51ae0692e732538d350cc
William Woods <willwoods04@icloud.com>
2025-04-20 17:19:36 -0700

    Setup the draw functions for the following 3 classes. Black Piece, Red Piece, and CheckerPiece.

--- CheckerPiece.hpp
+++ CheckerPiece.hpp
@@ -8,53 +8,60 @@ using std::vector;
 using std::get;
 using std::make_tuple;
 
-class CheckerPiece {
-    public:
-        CheckerPiece(int row, int col) :isKing(false) {
-            pos = make_tuple(row, col);
-        }
+class CheckerPiece : public sf::Drawable{
+public:
+    CheckerPiece(int row, int col) :isKing(false) {
+        pos = make_tuple(row, col);
+        circle.setRadius(46);
+        circle.setFillColor(sf::Color::Black);
+    }
 
-        void changePos(tuple<int, int> posChange) {
-            pos = make_tuple(get<0>(pos) + get<0>(posChange), get<1>(pos) + get<1>(posChange));
-        }
+    void changePos(tuple<int, int> posChange) {
+        pos = make_tuple(get<0>(pos) + get<0>(posChange), get<1>(pos) + get<1>(posChange));
+    }
 
-        tuple<int, int> getPos() {
-            return pos;
-        }
+    tuple<int, int> getPos() {
+        return pos;
+    }
 
-        bool isKingPiece() const {
-            return isKing;
-        }
+    bool isKingPiece() const {
+        return isKing;
+    }
 
-        void promoteToKing() {
-            isKing = true;
-        }
+    void promoteToKing() {
+        isKing = true;
+    }
 
-        virtual vector<tuple<int, int>> getMove() const {
-            if (isKing) {
-                return vector<tuple<int, int>>{
-                    {get<0>(pos) - 1, get<1>(pos) - 1},
-                    {get<0>(pos) - 1, get<1>(pos) + 1},
-                    {get<0>(pos) + 1, get<1>(pos) - 1},
-                    {get<0>(pos) + 1, get<1>(pos) + 1}
-                };
-            }
-            return {};
+    virtual vector<tuple<int, int>> getMove() const {
+        if (isKing) {
+            return vector<tuple<int, int>>{
+                            {get<0>(pos) - 1, get<1>(pos) - 1},
+                            {get<0>(pos) - 1, get<1>(pos) + 1},
+                            {get<0>(pos) + 1, get<1>(pos) - 1},
+                            {get<0>(pos) + 1, get<1>(pos) + 1}
+            };
         }
-        virtual vector<tuple<int, int>> getCapture() const {
-            if (isKing) {
-                return vector<tuple<int, int>>{
-                    {get<0>(pos) - 2, get<1>(pos) - 2},
-                    {get<0>(pos) - 2, get<1>(pos) + 2},
-                    {get<0>(pos) + 2, get<1>(pos) - 2},
-                    {get<0>(pos) + 2, get<1>(pos) + 2}
-                };
-            }
-            return {};
+        return {};
+    }
+
+    virtual vector<tuple<int, int>> getCapture() const {
+        if (isKing) {
+            return vector<tuple<int, int>>{
+                            {get<0>(pos) - 2, get<1>(pos) - 2},
+                            {get<0>(pos) - 2, get<1>(pos) + 2},
+                            {get<0>(pos) + 2, get<1>(pos) - 2},
+                            {get<0>(pos) + 2, get<1>(pos) + 2}
+            };
         }
+        return {};
+    }
 
+    void draw(sf::RenderTarget& target, sf::RenderStates states) const override {
+        target.draw(circle, states);
+    }
 
-    private:
-        bool isKing;
-        tuple<int, int> pos;
+protected:
+    sf::CircleShape circle;
+    bool isKing;
+    tuple<int, int> pos;
 };
--- RedPiece.hpp
+++ RedPiece.hpp
@@ -3,24 +3,30 @@
 #include "CheckerPiece.hpp"
 
 class RedPiece : public CheckerPiece {
-    public:
-        RedPiece(int row, int col) : CheckerPiece(row, col) {};
-        vector<tuple<int, int>> getMove() {
-            if (isKingPiece()) {
-                return CheckerPiece::getMove();
-            }
-            return vector<tuple<int, int>>{
-                {get<0>(getPos()) + 1, get<1>(getPos()) + 1},
-                {get<0>(getPos()) + 1, get<1>(getPos()) - 1}
-            };
+public:
+    RedPiece(int row, int col) : CheckerPiece(row, col) {
+        circle.setFillColor(sf::Color::Red);
+        circle.setOutlineThickness(2);
+        circle.setOutlineColor(sf::Color::White);
+        circle.setPosition(sf::Vector2f(4, 4));
+    };
+
+    vector<tuple<int, int>> getMove() {
+        if (isKingPiece()) {
+            return CheckerPiece::getMove();
         }
-        vector<tuple<int, int>> getCapture() {
-            if (isKingPiece()) {
-                return CheckerPiece::getCapture();
-            }
-            return vector<tuple<int, int>>{
-                {get<0>(getPos()) + 2, get<1>(getPos()) + 2},
-                {get<0>(getPos()) + 2, get<1>(getPos()) - 2}
-            };
+        return vector<tuple<int, int>>{
+                        {get<0>(getPos()) + 1, get<1>(getPos()) + 1},
+                        {get<0>(getPos()) + 1, get<1>(getPos()) - 1}
+        };
+    }
+    vector<tuple<int, int>> getCapture() {
+        if (isKingPiece()) {
+            return CheckerPiece::getCapture();
         }
+        return vector<tuple<int, int>>{
+                        {get<0>(getPos()) + 2, get<1>(getPos()) + 2},
+                        {get<0>(getPos()) + 2, get<1>(getPos()) - 2}
+        };
+    }
 };
