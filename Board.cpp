97d4d82c18a24709c5d68988b3186f69656bc9ff
laubenjamin <launumber4@gmail.com>
2025-04-22 17:03:17 -0700

    changed board

--- Board.cpp
+++ Board.cpp
@@ -1,4 +1,6 @@
 #include "Board.hpp"
+#include "BlackPiece.hpp"
+#include "RedPiece.hpp" 
 
 Board::Board() {
     const float squareSize = 100.0f; // Size of each square
@@ -13,11 +15,25 @@ Board::Board() {
             } else {
                 squares[row][col].setFillColor(sf::Color::Black);
             }
+
+            if (boardArray[row][col] == 'b') {
+                pieceArray[row][col] = new BlackPiece(row, col);
+            } else if (boardArray[row][col] == 'r') {
+                pieceArray[row][col] = new RedPiece(row, col);
+            } else {
+                pieceArray[row][col] = nullptr; // Empty square
+            }
         }
     }
 }
 
-Board::~Board() {}
+Board::~Board() {
+    for (int row = 0; row < 8; ++row) {
+        for (int col = 0; col < 8; ++col) {
+            delete pieceArray[row][col];
+        }
+    }
+}
 
 void Board::draw(sf::RenderTarget& target, sf::RenderStates states) const {
     for (int row = 0; row < 8; ++row) {
@@ -25,4 +41,12 @@ void Board::draw(sf::RenderTarget& target, sf::RenderStates states) const {
             target.draw(squares[row][col], states);
         }
     }
+    for (int row = 0; row < 8; ++row) {
+        for (int col = 0; col < 8; ++col) {
+            if (pieceArray[row][col] != nullptr) {
+                target.draw(*pieceArray[row][col], states);
+            }
+        }
+    }
 }
+
--- Board.hpp
+++ Board.hpp
@@ -1,12 +1,35 @@
 #include <SFML/Graphics.hpp>
+#include "CheckerPiece.hpp"
 
 class Board : public sf::Drawable {
 public:
     Board();
     ~Board();
     void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
+    CheckerPiece* getPieceAt(int row, int col) {
+        return pieceArray[row][col];
+    }
+    void setFutureMoves(vector<tuple<int, int>> moves) {
+        futureMoves = moves;
+    }
+    void setCurPiece(CheckerPiece* piece) {
+        curPiece = piece;
+    }
+    CheckerPiece* getCurPiece() {
+        return curPiece;
+    }
+    bool capturePiece(CheckerPiece* piece, int row, int col) {
+        if (pieceArray[row][col] != nullptr) {
+            delete pieceArray[row][col];
+            pieceArray[row][col] = nullptr;
+            return true;
+        }
+        return false;
+    }
 
 private:
+    vector<tuple<int, int>> futureMoves;
+    CheckerPiece* curPiece = nullptr;
     sf::RectangleShape squares[8][8]; // 8x8 grid of squares
     char boardArray [8][8] = {
         {' ', 'b', ' ', 'b', ' ', 'b', ' ', 'b'},
@@ -18,4 +41,5 @@ private:
         {' ', 'r', ' ', 'r', ' ', 'r', ' ', 'r'},
         {'r', ' ', 'r', ' ', 'r', ' ', 'r', ' '}
     };
+    CheckerPiece* pieceArray [8][8] = {nullptr};
 };
