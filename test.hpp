47ac09c85291ebd3c8ab7bcae3bca2c4042eba4a
laubenjamin <launumber4@gmail.com>
2025-04-23 22:20:48 -0700

    updated

--- test.hpp
+++ test.hpp
@@ -5,82 +5,69 @@
 #include "RedPiece.hpp"
 #include "BlackPiece.hpp"
 
+using std::cout;
+using std::endl;
+
 class TestCase {
     public:
         void printTestResult(const std::string& testName, bool result) {
             if (result) {
-                std::cout << "[PASS] " << testName << std::endl;
+                cout << "[PASS] " << testName << endl;
             } else {
-                std::cout << "[FAIL] " << testName << std::endl;
+                cout << "[FAIL] " << testName << endl;
             }
         }
         
         void testCheckerPieceInitialization() {
             RedPiece redPiece(5, 2);
             BlackPiece blackPiece(2, 3);
-        
-            assert(redPiece.getPos() == std::make_tuple(5, 2));
-            assert(blackPiece.getPos() == std::make_tuple(2, 3));
-        
+            assert(redPiece.getPos() == make_tuple(5, 2));
+            assert(blackPiece.getPos() == make_tuple(2, 3));
             assert(redPiece.getisRed());
             assert(!blackPiece.getisRed());
-        
             printTestResult("CheckerPiece Initialization", true);
         }
         
         void testCheckerPieceKingPromotion() {
             RedPiece redPiece(0, 2);
             BlackPiece blackPiece(7, 3);
-        
             assert(!redPiece.isKingPiece());
             assert(!blackPiece.isKingPiece());
-        
-            redPiece.changePos(std::make_tuple(0, 2));
-            blackPiece.changePos(std::make_tuple(7, 3));
-        
+            redPiece.changePos(make_tuple(0, 2));
+            blackPiece.changePos(make_tuple(7, 3));
             assert(redPiece.isKingPiece());
             assert(blackPiece.isKingPiece());
-        
             printTestResult("CheckerPiece King Promotion", true);
         }
         
         void testBoardInitialization() {
             Board board;
-        
             assert(board.getNumRedPieces() == 12);
             assert(board.getNumBlackPieces() == 12);
-        
             assert(board.getPieceAt(0, 1) != nullptr);
             assert(board.getPieceAt(7, 0) != nullptr);
             assert(board.getPieceAt(3, 3) == nullptr);
-        
             printTestResult("Board Initialization", true);
         }
         
         void testBoardMovePiece() {
             Board board;
-        
             CheckerPiece* piece = board.getPieceAt(5, 0);
             assert(piece != nullptr);
-        
             board.setCurPiece(piece);
             board.movePiece(4, 1);
-        
             assert(board.getPieceAt(5, 0) == nullptr);
             assert(board.getPieceAt(4, 1) == piece);
-        
             printTestResult("Board Move Piece", true);
         }
         
         void testBoardChangeTurn() {
             Board board;
-        
             assert(board.getCurTurn());
             board.changeTurn();
             assert(!board.getCurTurn());
             board.changeTurn();
             assert(board.getCurTurn());
-        
             printTestResult("Board Change Turn", true);
         }
         
